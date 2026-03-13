eaf954e4232858d5ee1789f4898ad60b267dfbe8
Azkona05 <135417707+Azkona05@users.noreply.github.com>
2026-03-13 10:02:53 +0100

    actualizado

--- src/vista/VMenuAdmin.java
+++ src/vista/VMenuAdmin.java
@@ -1,17 +1,19 @@
 package vista;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
-public class VMenuAdmin extends JDialog {
+public class VMenuAdmin extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JButton btnBaja, btnAlta, btnModificar, btnConsultar;
 
 	/**
 	 * Create the dialog.
@@ -21,25 +23,37 @@ public class VMenuAdmin extends JDialog {
 		setModal(modal);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
+		contentPanel.setLayout(null);
+
+		btnAlta = new JButton("ALTA");
+		btnAlta.setBounds(60, 62, 128, 39);
+		contentPanel.add(btnAlta);
+
+		btnBaja = new JButton("BAJA");
+		btnBaja.setBounds(256, 62, 128, 39);
+		contentPanel.add(btnBaja);
+
+		btnModificar = new JButton("MODIFICAR");
+		btnModificar.setBounds(60, 170, 128, 39);
+		contentPanel.add(btnModificar);
+		
+		btnConsultar = new JButton("CONSULTAR");
+		btnConsultar.setBounds(256, 170, 128, 39);
+		contentPanel.add(btnConsultar);
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(btnAlta)) {
+			
+		} else if(e.getSource().equals(btnBaja)) {
+			
+		} else if(e.getSource().equals(btnModificar)) {
+			
+		} else if(e.getSource().equals(btnConsultar)) {
+			
+		}
+	}
 }
