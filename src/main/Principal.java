97142c38394dad62f0011d07cfb0079737cc4e8f
Azkona05 <135417707+Azkona05@users.noreply.github.com>
2026-04-16 09:14:28 +0200

    errores fixeados

--- src/main/Principal.java
+++ src/main/Principal.java
@@ -54,13 +54,13 @@ public class Principal {
 	public static Object[][] devolverCancionesArtista(Artista a) throws LoginException {
 		return dao.devolverCancionesArtista(a);
 	}
-	
+
 	// AN
 	public static Object[][] devolverAlbumesT() throws LoginException {
 		return dao.devolverAlbumesT();
 	}
 
-	//NORA
+	// NORA
 	public static boolean eliminarCancion(int idC) throws SQLException {
 		boolean resultado = dao.eliminarCancion(idC);
 		if (resultado) {
@@ -69,7 +69,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//NORA
+	// NORA
 	public static boolean eliminarArtista(int idArt) throws SQLException {
 		boolean resultado = dao.eliminarArtista(idArt);
 		if (resultado) {
@@ -78,7 +78,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//NORA
+	// NORA
 	public static boolean eliminarAlbum(int idAlb) throws SQLException {
 		boolean resultado = dao.eliminarAlbum(idAlb);
 		if (resultado) {
@@ -87,7 +87,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean alta(Artista artista) throws AltaException {
 		boolean resultado = dao.altaArtista(artista);
 		if (resultado) {
@@ -96,7 +96,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean existeCancionEnAlbum(String nombreCancion, int idAlbum) throws AltaException {
 		return dao.existeCancionEnAlbum(nombreCancion, idAlbum);
 	}
@@ -104,54 +104,62 @@ public class Principal {
 //		return dao.leerDatosArtista();
 //	}
 
-	//RICARDO
+	// RICARDO
 	public static ArrayList<Integer> leerIds() throws AltaException {
 		return dao.ides();
 
 	}
 
-	//RICARDO
+	// RICARDO
 	public static ArrayList<String> leerNombreArti() throws AltaException {
 		return dao.nomArti();
 	}
 
-	//RICARDO
+	// RICARDO
 	public static List<Artista> obtenerTodosLosArtistasCompletos() throws LoginException {
 		return dao.obtenerTodosLosArtistasCompletos();
 
 	}
-	
+
 	// AN
 	public static Object[][] devolverCanciones() throws LoginException {
 		return dao.devolverCanciones();
 	}
-	
-	//RICARDO
+
+	// RICARDO
 	public static Map<Integer, Artista> listarArti(Artista arti) throws AltaException {
 		return dao.listarArtTabla(arti);
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean altaAlbum(Album album, int idArtista) throws AltaException {
-		return dao.altaAlbum(album, idArtista);
+		boolean resultado = dao.altaAlbum(album, idArtista);
+		if (resultado) {
+			dao.forzarGuardadoXML();
+		}
+		return resultado;
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean altaCancion(Cancion cancion, int idAlbum) throws AltaException {
-		return dao.altaCancion(cancion, idAlbum);
+		boolean resultado = dao.altaCancion(cancion, idAlbum);
+		if (resultado) {
+			dao.forzarGuardadoXML();
+		}
+		return resultado;
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean existeIdAlbum(int id) throws AltaException {
 		return dao.existeIdAlbum(id);
 	}
 
-	//RICARDO
+	// RICARDO
 	public static boolean existeIdCancion(int id) throws AltaException {
 		return dao.existeIdCancion(id);
 	}
 
-	//JON ANDER
+	// JON ANDER
 	public static boolean modificarArtista(int id, String nombre, String tipo) throws SQLException {
 		boolean resultado = dao.modificarArtista(id, nombre, tipo);
 		if (resultado) {
@@ -160,7 +168,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//JON ANDER
+	// JON ANDER
 	public static boolean modificarAlbum(int id, String nombre, int idArtista) throws SQLException {
 		boolean resultado = dao.modificarAlbum(id, nombre, idArtista);
 		if (resultado) {
@@ -169,7 +177,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//JON ANDER
+	// JON ANDER
 	public static boolean modificarCancion(int id, String nombre, String genero, int idAlbum) throws SQLException {
 		boolean resultado = dao.modificarCancion(id, nombre, genero, idAlbum);
 		if (resultado) {
@@ -178,7 +186,7 @@ public class Principal {
 		return resultado;
 	}
 
-	//JON ANDER
+	// JON ANDER
 	public static String tipoAlbum(int idAlbum) throws SQLException {
 		return dao.tipoAlbum(idAlbum);
 	}
